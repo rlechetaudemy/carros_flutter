00753f709c2d02e1e95ec237c4a3ef1dc43dd70b
rlecheta <rlecheta@livetouch.com.br>
2019-07-13 11:54:35 -0300

    ok

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  final _tLogin = TextEditingController();
-  final _tSenha = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  final _tLogin = TextEditingController(text: "ricardo");
+  final _tSenha = TextEditingController(text: "123");
 
   @override
   Widget build(BuildContext context) {
@@ -15,28 +17,37 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: <Widget>[
-          _text("Login", "Digite o login", controller: _tLogin),
-          SizedBox(
-            height: 10
-          ),
-          _text("Senha", "Digite a senha", controller: _tSenha, password: true),
-          SizedBox(
-            height: 20,
-          ),
-          _button("Login", _onClickLogin),
-        ],
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            _text("Login", "Digite o login", controller: _tLogin, validator: _validateLogin),
+            SizedBox(height: 10),
+            _text("Senha", "Digite a senha",
+                controller: _tSenha, password: true, validator: _validateSenha),
+            SizedBox(
+              height: 20,
+            ),
+            _button("Login", _onClickLogin),
+          ],
+        ),
       ),
     );
   }
 
-  _text(String label, String hint, {bool password = false, TextEditingController controller}) {
+  _text(
+    String label,
+    String hint, {
+    bool password = false,
+    TextEditingController controller,
+    FormFieldValidator<String> validator,
+  }) {
     return TextFormField(
       controller: controller,
       obscureText: password,
+      validator: validator,
       style: TextStyle(
         fontSize: 25,
         color: Colors.blue,
@@ -73,9 +84,31 @@ class LoginPage extends StatelessWidget {
   }
 
   void _onClickLogin() {
+
+    if (! _formKey.currentState.validate()) {
+      return;
+    }
+
     String login = _tLogin.text;
     String senha = _tSenha.text;
 
     print("Login: $login, Senha: $senha");
+  }
+
+  String _validateLogin(String text) {
+    if (text.isEmpty) {
+      return "Digite o login";
+    }
+    return null;
+  }
+
+  String _validateSenha(String text) {
+    if (text.isEmpty) {
+      return "Digite a senha";
+    }
+    if(text.length < 3) {
+      return "A senha precisa ter pelo menos 3 números";
+    }
+    return null;
   }
 }
