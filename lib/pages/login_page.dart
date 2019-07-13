c02d5ba6a6c650939b6f153b8efd813ee3ad71b7
rlecheta <rlecheta@livetouch.com.br>
2019-07-13 10:34:03 -0300

    First commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +17,41 @@ class LoginPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: ListView(
         children: <Widget>[
-          Text("Login"),
-          TextFormField(),
+          TextFormField(
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.blue
+            ),
+            decoration: InputDecoration(
+              labelText: "Login",
+              labelStyle: TextStyle(
+                fontSize: 25,
+                color: Colors.grey
+              ),
+              hintText: "Digite o login",
+              hintStyle: TextStyle(
+                fontSize: 16
+              )
+            ),
+          ),
           SizedBox(height: 10,),
-          Text("Senha"),
           TextFormField(
             obscureText: true,
+            style: TextStyle(
+                fontSize: 25,
+                color: Colors.blue
+            ),
+            decoration: InputDecoration(
+                labelText: "Senha",
+                labelStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.grey
+                ),
+                hintText: "Digite a senha",
+                hintStyle: TextStyle(
+                    fontSize: 16
+                )
+            ),
           ),
           SizedBox(height: 20,),
           Container(
