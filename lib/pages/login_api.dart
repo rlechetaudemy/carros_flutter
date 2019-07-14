bdd3a6a0587a57bd6e974d968a6db80c29341420
rlecheta <rlecheta@livetouch.com.br>
2019-07-14 17:53:02 -0300

    login

--- lib/pages/login_api.dart
+++ lib/pages/login_api.dart
@@ -1,15 +1,23 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class LoginApi {
   static Future<bool> login(String login, String senha) async {
-    var url = 'http://livrowebservices.com.br/rest/login';
+    var url = 'https://carros-springboot.herokuapp.com/api/v2/login';
+
+    Map<String,String> headers = {
+      "Content-Type": "application/json"
+    };
 
     Map params = {
       "username": login,
       "password": senha
     };
 
-    var response = await http.post(url, body: params);
+    String s = json.encode(params);
+    print("> $s");
+
+    var response = await http.post(url, body: s, headers: headers);
 
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
