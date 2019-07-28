417d0a77edbcb9102b08d593834ea83b16578b64
rlecheta <rlecheta@livetouch.com.br>
2019-07-28 15:04:21 -0300

    home

--- lib/pages/login/login_api.dart
+++ lib/pages/login/login_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:carros/pages/api_response.dart';
 import 'package:carros/pages/login/usuario.dart';
+import 'package:carros/utils/prefs.dart';
 import 'package:http/http.dart' as http;
 
 class LoginApi {
@@ -29,6 +30,11 @@ class LoginApi {
 
       if(response.statusCode == 200) {
         final user = Usuario.fromJson(mapResponse);
+
+        user.save();
+
+        Usuario user2 = await Usuario.get();
+        print("user2: $user2");
 
         return ApiResponse.ok(user);
       }
--- lib/pages/login/usuario.dart
+++ lib/pages/login/usuario.dart
@@ -1,22 +1,56 @@
+import 'package:carros/utils/prefs.dart';
+import 'dart:convert' as convert;
+
 class Usuario {
   String login;
   String nome;
   String email;
+  String urlFoto;
   String token;
-
   List<String> roles;
 
-  Usuario.fromJson(Map<String, dynamic> map)
-      : login = map["login"],
-        nome = map["nome"],
-        email = map["email"],
-        token = map["token"],
-        roles = map["roles"] != null
-            ? map["roles"].map<String>((role) => role.toString()).toList()
-            : null;
+  Usuario(
+      {this.login,
+        this.nome,
+        this.email,
+        this.urlFoto,
+        this.token,
+        this.roles});
 
-  @override
-  String toString() {
-    return 'Usuario{login: $login, nome: $nome, email: $email, token: $token, roles: $roles}';
+  Usuario.fromJson(Map<String, dynamic> json) {
+    login = json['login'];
+    nome = json['nome'];
+    email = json['email'];
+    urlFoto = json['urlFoto'];
+    token = json['token'];
+    roles = json['roles'].cast<String>();
   }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['login'] = this.login;
+    data['nome'] = this.nome;
+    data['email'] = this.email;
+    data['urlFoto'] = this.urlFoto;
+    data['token'] = this.token;
+    data['roles'] = this.roles;
+    return data;
+  }
+
+  void save() {
+    Map map = toJson();
+
+    String json = convert.json.encode(map);
+
+    Prefs.setString("user.prefs", json);
+  }
+
+  static Future<Usuario> get() async {
+    String json = await Prefs.getString("user.prefs");
+    Map map = convert.json.decode(json);
+    Usuario user = Usuario.fromJson(map);
+    return user;
+  }
+
+
 }
