f0bae05684527a41de6035e32bb13c0e19d41a77
rlecheta <rlecheta@livetouch.com.br>
2019-08-03 11:31:33 -0300

    bloc

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -9,10 +9,40 @@ class CarroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(carro.nome),
-        ),
-        body: _body());
+      appBar: AppBar(
+        title: Text(carro.nome),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: _onClickMapa,
+          ),
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: _onClickVideo,
+          ),
+          PopupMenuButton<String>(
+            onSelected: _onClickPopupMenu,
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  value: "Editar",
+                  child: Text("Editar"),
+                ),
+                PopupMenuItem(
+                  value: "Deletar",
+                  child: Text("Deletar"),
+                ),
+                PopupMenuItem(
+                  value: "Share",
+                  child: Text("Share"),
+                )
+              ];
+            },
+          )
+        ],
+      ),
+      body: _body(),
+    );
   }
 
   _body() {
@@ -20,5 +50,23 @@ class CarroPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: Image.network(carro.urlFoto),
     );
+  }
+
+  void _onClickMapa() {}
+
+  void _onClickVideo() {}
+
+  _onClickPopupMenu(String value) {
+    switch (value) {
+      case "Editar":
+        print("Editar !!!");
+        break;
+      case "Deletar":
+        print("Deletar !!!");
+        break;
+      case "Share":
+        print("Share !!!");
+        break;
+    }
   }
 }
