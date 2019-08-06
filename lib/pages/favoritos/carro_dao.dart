ecd3db1013596da830a304276e07fc9e03f633f8
rlecheta <rlecheta@livetouch.com.br>
2019-08-06 19:52:31 -0300

    sqlite

---
+++ lib/pages/favoritos/carro_dao.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:carros/pages/carros/carro.dart';
+import 'package:carros/pages/favoritos/db_helper.dart';
+import 'package:sqflite/sqflite.dart';
+
+class CarroDAO {
+
+  get db => DatabaseHelper.getInstance().db;
+
+  Future<int> save(Carro carro) async {
+    var dbClient = await db;
+    var id = await dbClient.insert("carro", carro.toJson());
+    print('id: $id');
+    return id;
+  }
+
+  Future<List<Carro>> findAll() async {
+    final dbClient = await db;
+
+    final list = await dbClient.rawQuery('select * from carro');
+
+    final carros = list.map<Carro>((json) => Carro.fromJson(json)).toList();
+
+    return carros;
+  }
+
+  Future<Carro> findById(int id) async {
+    var dbClient = await db;
+    final list =
+        await dbClient.rawQuery('select * from carro where id = ?', [id]);
+
+    if (list.length > 0) {
+      return new Carro.fromJson(list.first);
+    }
+
+    return null;
+  }
+
+  Future<bool> exists(Carro carro) async {
+    Carro c = await findById(carro.id);
+    var exists = c != null;
+    return exists;
+  }
+
+  Future<int> count() async {
+    final dbClient = await db;
+    final list = await dbClient.rawQuery('select count(*) from carro');
+    return Sqflite.firstIntValue(list);
+  }
+
+  Future<int> delete(int id) async {
+    var dbClient = await db;
+    return await dbClient.rawDelete('delete from carro where id = ?', [id]);
+  }
+}
---
+++ lib/pages/favoritos/db_helper.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper.getInstance();
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper.getInstance();
+
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDb();
+
+    return _db;
+  }
+
+  initDb() async {
+    String databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'carros.db');
+    print("db $path");
+
+    // para testes vc pode deletar o banco
+//    await deleteDatabase(path);
+
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate, onUpgrade: _onUpgrade);
+    return db;
+  }
+
+  void _onCreate(Database db, int newVersion) async {
+    await db.execute(
+        'CREATE TABLE carro(id INTEGER PRIMARY KEY, tipo TEXT, nome TEXT'
+        ', descricao TEXT, urlFoto TEXT, urlVideo TEXT, latitude TEXT, longitude TEXT)');
+  }
+
+  FutureOr<void> _onUpgrade(Database db, int oldVersion, int newVersion) {
+    print("_onUpgrade: oldVersion: $oldVersion > newVersion: $newVersion");
+  }
+
+  Future close() async {
+    var dbClient = await db;
+    return dbClient.close();
+  }
+}
