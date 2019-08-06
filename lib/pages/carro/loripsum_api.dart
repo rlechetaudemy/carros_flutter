dc6a6941eae8daacfe8a4a5a357b6805fca41b58
rlecheta <rlecheta@livetouch.com.br>
2019-08-06 20:13:35 -0300

    sqlite

--- lib/pages/carro/loripsum_api.dart
+++ lib/pages/carro/loripsum_api.dart
@@ -4,12 +4,16 @@ import 'package:http/http.dart' as http;
 
 class LoripsumBloc {
 
+  static String lorim;
+
   final _streamController = StreamController<String>();
 
   Stream<String> get stream => _streamController.stream;
 
   fetch() async {
-    String s = await LoripsumApi.getLoripsum();
+    String s = lorim ?? await LoripsumApi.getLoripsum();
+
+    lorim = s;
 
     _streamController.add(s);
   }
