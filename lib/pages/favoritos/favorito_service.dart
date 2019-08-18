8d31fdde49fa2497d27140c2e90efb1f1dee9d0f
rlecheta <rlecheta@livetouch.com.br>
2019-08-18 12:00:39 -0300

    cache

---
+++ lib/pages/favoritos/favorito_service.dart
@@ -0,0 +1,23 @@
+
+import 'package:carros/pages/carros/carro.dart';
+import 'package:carros/pages/favoritos/favorito.dart';
+import 'package:carros/pages/favoritos/favorito_dao.dart';
+
+class FavoritoService {
+  static favoritar(Carro c) async {
+
+    Favorito f = Favorito.fromCarro(c);
+
+    final dao = FavoritoDAO();
+
+    final exists = await dao.exists(c.id);
+
+    if(exists) {
+      // Remove dos favoritos
+      dao.delete(c.id);
+    } else {
+      // Adiciona nos favoritos
+      dao.save(f);
+    }
+  }
+}
