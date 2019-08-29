79708419aff189f99f6129a4624816bf7b05362d
rlecheta <rlecheta@livetouch.com.br>
2019-08-29 20:55:46 -0300

    ok

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/rlecheta/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/rlecheta/git/Udemy/carros"
+export "FLUTTER_TARGET=/Users/rlecheta/git/Udemy/carros/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/rlecheta/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
