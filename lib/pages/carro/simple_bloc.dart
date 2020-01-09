9ae29a8b4c542e4f1a997fa682785be1a27a8211
rlecheta <rlecheta@livetouch.com.br>
2020-01-09 16:19:07 -0300

    SimpleBloc

--- lib/pages/carro/simple_bloc.dart
+++ lib/pages/carro/simple_bloc.dart
@@ -8,7 +8,9 @@ class SimpleBloc<T> {
   Stream<T> get stream => _controller.stream;
 
   void add(T object) {
-    _controller.add(object);
+    if(! _controller.isClosed) {
+      _controller.add(object);
+    }
   }
 
   void addError(Object error) {
