1c62618517ee8d5fef0c48e02cd9a2b06060595d
rlecheta <rlecheta@livetouch.com.br>
2020-01-16 09:10:46 -0300

    Stream - simples

--- lib/pages/carro/carros_bloc.dart
+++ lib/pages/carro/carros_bloc.dart
@@ -1,26 +1,22 @@
 
 import 'dart:async';
 
-import 'package:carros/pages/carro/carro.dart';
-import 'package:carros/pages/carro/carros_api.dart';
+import 'package:carros/pages/carros/carro.dart';
+import 'package:carros/pages/carros/carros_api.dart';
 
-class CarrosBloc {
+import '../../utils/simple_bloc.dart';
 
-  final _streamController = StreamController<List<Carro>>();
+class CarrosBloc extends SimpleBloc<List<Carro>>{
 
-  Stream<List<Carro>> get stream => _streamController.stream;
-
-  fetch(String tipo) async {
+  Future<List<Carro>> fetch(String tipo) async {
     try {
       List<Carro> carros = await CarrosApi.getCarros(tipo);
 
-      _streamController.add(carros);
-    } catch (e) {
-      _streamController.addError(e);
-    }
-  }
+      add(carros);
 
-  void dispose() {
-    _streamController.close();
+      return carros;
+    } catch (e) {
+      addError(e);
+    }
   }
 }
