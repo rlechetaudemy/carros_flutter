e65b607f5d70afa2158fefca62f5a59c7ab1d384
rlecheta <rlecheta@livetouch.com.br>
2020-01-16 10:15:53 -0300

    Stream - simples

--- lib/pages/login/login_bloc.dart
+++ lib/pages/login/login_bloc.dart
@@ -1,27 +1,22 @@
 
 import 'dart:async';
 
-import 'package:carros/firebase/firebase_service.dart';
 import 'package:carros/pages/api_response.dart';
+import 'package:carros/pages/login/login_api.dart';
+import 'package:carros/pages/login/usuario.dart';
 
-class LoginBloc {
-  final _streamController = StreamController<bool>();
+import 'package:carros/utils/simple_bloc.dart';
 
-  get stream => _streamController.stream;
+class LoginBloc extends BooleanBloc {
 
-  Future<ApiResponse> login(String login, String senha) async {
+  Future<ApiResponse<Usuario>> login(String login, String senha) async {
 
-    _streamController.add(true);
+    add(true);
 
-//    ApiResponse response = await LoginApi.login(login, senha);
-    ApiResponse response = await FirebaseService().login(login, senha);
+    ApiResponse response = await LoginApi.login(login, senha);
 
-    _streamController.add(false);
+    add(false);
 
     return response;
-  }
-
-  void dispose() {
-    _streamController.close();
   }
 }
