a08a97f284bf1ed55f52aa18abe96a0a753c4718
rlecheta <rlecheta@livetouch.com.br>
2020-01-16 10:11:56 -0300

    Stream - simples

--- lib/pages/carros/carros_bloc.dart
+++ lib/pages/carros/carros_bloc.dart
@@ -2,46 +2,21 @@
 import 'dart:async';
 
 import 'package:carros/pages/carros/carro.dart';
-import 'package:carros/pages/carros/carro_dao.dart';
 import 'package:carros/pages/carros/carros_api.dart';
-import 'package:carros/utils/network.dart';
 
-class CarrosBloc {
+import 'package:carros/utils/simple_bloc.dart';
 
-  final _streamController = StreamController<List<Carro>>();
-
-  Stream<List<Carro>> get stream => _streamController.stream;
+class CarrosBloc extends SimpleBloc<List<Carro>>{
 
   Future<List<Carro>> fetch(String tipo) async {
     try {
-
-      if(! await isNetworkOn()) {
-        // Busca do banco de dados
-        List<Carro> carros =  await CarroDAO().findAllByTipo(tipo);
-        _streamController.add(carros);
-        return carros;
-      }
-
       List<Carro> carros = await CarrosApi.getCarros(tipo);
 
-      if(carros.isNotEmpty) {
-        final dao = CarroDAO();
-
-        // Salvar todos os carros
-        carros.forEach(dao.save);
-      }
-
-      _streamController.add(carros);
+      add(carros);
 
       return carros;
     } catch (e) {
-      if(! _streamController.isClosed) {
-        _streamController.addError(e);
-      }
+      addError(e);
     }
-  }
-
-  void dispose() {
-    _streamController.close();
   }
 }
