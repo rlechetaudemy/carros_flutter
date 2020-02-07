eb2f6242c473a800c3c23476bd54a8795a040bac
rlecheta <rlecheta@livetouch.com.br>
2020-02-07 10:02:01 -0300

    Fix tab home pages

--- lib/pages/carros/carros_bloc.dart
+++ lib/pages/carros/carros_bloc.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 
 import 'package:carros/pages/carros/carro.dart';
+import 'package:carros/pages/carros/carro_dao.dart';
 import 'package:carros/pages/carros/carros_api.dart';
 
 import 'package:carros/utils/simple_bloc.dart';
@@ -13,6 +14,10 @@ class CarrosBloc extends SimpleBloc<List<Carro>>{
       List<Carro> carros = await CarrosApi.getCarros(tipo);
 
       add(carros);
+
+      // Salvar todos os carros no banco
+      final dao = CarroDAO();
+      carros.forEach(dao.save);
 
       return carros;
     } catch (e) {
